a16032325266d0deb5dfc8530763058ef4eac34c
Farkens <bulkov_olegandre@mail.ru>
2019-03-23 23:04:02 +0500

    crated table

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'sdfgojsdigfsiafTASKsadfkg34508nc0943u2x$',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
---
+++ migrations/m190323_180002_create_popup_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%popup}}`.
+ */
+class m190323_180002_create_popup_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function Up()
+    {
+        $this->createTable('{{popup}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'text' => $this->text(),
+            'is_active' => 'ENUM("on", "off")',
+            'count_show' => $this->integer()->defaultValue(0)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function Down()
+    {
+        $this->dropTable('{{%popup}}');
+    }
+}
